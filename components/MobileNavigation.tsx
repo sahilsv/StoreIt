78360c3cccfe9c9c5b89a2462d299f109abe1ea0
Sahil Singh Virdhi <sahilsv003@gmail.com>
2024-11-24 22:38:19 +0530

    Authentication done

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -3,8 +3,6 @@
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
@@ -17,6 +15,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import FileUploader from "./FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
   ownerId: string;
@@ -101,7 +100,7 @@ const MobileNavigation = ({
           <Separator className="my-5 bg-light-200/20" />
           <div className="flex flex-col justify-between gap-5 pb-5">
             <FileUploader />
-            <Button type="submit" className="mobile-sign-out-button" onClick={() => {}}>
+            <Button type="submit" className="mobile-sign-out-button" onClick={async () => await signOutUser()}>
               <Image
                 src="/assets/icons/logout.svg"
                 alt="logo"
